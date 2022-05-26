3fd3b65a07c9c6dcfa3a3ea400c3464b5e2c3703
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-05-27 00:53:01 +0300

    add data files

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import Profile from 'components/Profile/Profile';
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+}
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import { css } from '@emotion/css';
+// import PropTypes from 'prop-types';
+// import { SECONDARY_COLOR } from '../colors';
+
+const Profile = props => {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">Petra Marica</p>
+        <p class="tag">@pmarica</p>
+        <p class="location">Salvador, Brasil</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+// Link.propTypes = {
+//   url: PropTypes.string.isRequired,
+//   children: PropTypes.node.isRequired,
+// };
+
+export default Profile;
